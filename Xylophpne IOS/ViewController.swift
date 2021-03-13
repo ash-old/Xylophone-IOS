33bfc52963aae32172ace92176fc2362e6636014
ash-old <ashley.oldham@gmail.com>
2021-03-13 22:14:04 +0000

    IB action linked to all buttons. When clicked, the current note is printed to the console.

--- Xylophpne IOS/ViewController.swift
+++ Xylophpne IOS/ViewController.swift
@@ -6,14 +6,32 @@
 //
 
 import UIKit
+import AVFoundation
+
 
 class ViewController: UIViewController {
+    
+    var audioPlayer: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    
+    @IBAction func keyPressed(_ sender: UIButton) {
+        
+        let soundName = sender.currentTitle
+        print(soundName)
+//        playSound(soundName: soundName!)
+    }
+    
+    
+//    func playSound(soundName: String) {
+//        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+//        audioPlayer = try! AVAudioPlayer(contentsOf: url!)
+//        audioPlayer.play()
+//    }
 
 }
 
