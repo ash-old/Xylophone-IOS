84d855b3b41f9e97eb9594f28adf67cd935c07b3
ash-old <ashley.oldham@gmail.com>
2021-03-14 00:14:28 +0000

    fixed error with sounds not playing.

--- Xylophpne IOS/ViewController.swift
+++ Xylophpne IOS/ViewController.swift
@@ -21,16 +21,22 @@ class ViewController: UIViewController {
     
     @IBAction func keyPressed(_ sender: UIButton) {
         
+        sender.alpha = 0.5
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2){
+            sender.alpha = 1.0
+        }
+        
         let soundName = sender.currentTitle
+        print(soundName!)
         playSound(soundName: soundName!)
     }
-    
     
     func playSound(soundName: String) {
         let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
         player = try! AVAudioPlayer(contentsOf: url!)
         player.play()
     }
+    
 
 }
 
