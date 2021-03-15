9e84ebc2bbff084c5386dcee85e17ce1f39913a1
ash-old <ashley.oldham@gmail.com>
2021-03-15 12:17:19 +0000

    final commit. App works on iPhone simulator

--- Xylophpne IOS/ViewController.swift
+++ Xylophpne IOS/ViewController.swift
@@ -31,6 +31,7 @@ class ViewController: UIViewController {
         playSound(soundName: soundName!)
     }
     
+    
     func playSound(soundName: String) {
         let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
         player = try! AVAudioPlayer(contentsOf: url!)
